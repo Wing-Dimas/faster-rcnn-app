5723956a9b340b39f05061d59193ec3030766b34
Wing-Dimas <wingdimas@gmail.com>
2024-07-10 11:36:22 +0700

    testing production 1

--- frontend/src/ApiManager/ApiManager.ts
+++ frontend/src/ApiManager/ApiManager.ts
@@ -1,26 +1,25 @@
 import axios, { AxiosResponse } from "axios";
 import ENDPOINTS from "./EndPoints";
-import { appConfig } from "@/config";
+// import { appConfig } from "@/config";
 
 // const BASE_URL: string = appConfig.apiURL;
-const apiRequest = axios.create({
-  baseURL: appConfig.apiURL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// const apiRequest = axios.create({
+//   baseURL: appConfig.apiURL,
+// });
 
 class ApiManager {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   static predictImage = (data: FormData, model: string): Promise<AxiosResponse<any, any>> => {
-    const isDev = appConfig.env === "local";
-    if (isDev) {
-      const url = "/api" + ENDPOINTS.PREDICT_IMAGE(model);
-      return axios.post(url, data);
-    } else {
-      const url = ENDPOINTS.PREDICT_IMAGE(model);
-      return apiRequest.post(url, data);
-    }
+    const url = "/api" + ENDPOINTS.PREDICT_IMAGE(model);
+    return axios.post(url, data);
+    // const isDev = appConfig.env === "local";
+    // if (isDev) {
+    //   const url = "/api" + ENDPOINTS.PREDICT_IMAGE(model);
+    //   return axios.post(url, data);
+    // } else {
+    //   const url = ENDPOINTS.PREDICT_IMAGE(model);
+    //   return apiRequest.post(url, data);
+    // }
   };
 }
 
