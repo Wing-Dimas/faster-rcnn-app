ae32355f262c888cb282426f36b33ec9572a0adf
Wing-Dimas <wingdimas@gmail.com>
2024-07-10 07:48:52 +0700

    fix: error in production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,9 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode, command }) => {
+export default defineConfig(({ mode }) => {
   //   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   const env = loadEnv(mode, process.cwd(), "");
-  console.log(mode);
-  console.log(env.VITE_API_URL);
   return {
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
